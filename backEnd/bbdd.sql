d8c52cc0bc0b692ed22e1e6dff97ccccbfea3509
Diego Carvajal MTrabajo <diego.carvajal.1515@gmail.com>
2023-06-12 11:48:56 -0400

    Archivo necesario para crear bbdd

--- backEnd/bbdd.sql
+++ backEnd/bbdd.sql
@@ -1,14 +1,37 @@
 CREATE DATABASE likeme;
-CREATE TABLE posts (id SERIAL, titulo VARCHAR(25), img VARCHAR(1000),descripcion VARCHAR(255), likes INT);
-INSERT INTO posts
- values (DEFAULT, 'hola', 'https://w7.pngwing.com/pngs/59/451/png-transparent-text-comics-speech-balloon-drawing-%C2%A1hola-comic-bubbles-comics-text-logo-thumbnail.png', 'hola', '1');
+CREATE TABLE posts (
+    id SERIAL,
+    titulo VARCHAR(25),
+    img VARCHAR(1000),
+    descripcion VARCHAR(255),
+    likes INT
+);
 
-UPDATE posts
-    SET likes = 0
-    where likes = 1
-    AND id = 1;
+DELETE FROM posts;
 
-DELETE FROM posts
-    where id = ;
+INSERT INTO posts (titulo, img, descripcion, likes)
+VALUES (
+        'Nike Store',
+        'https://www.svgrepo.com/show/303214/nike-4-logo.svg',
+        'Tienda oficial de Nike',
+        5000
+    );
+INSERT INTO posts (titulo, img, descripcion, likes)
+VALUES (
+        'Adidas Outlet',
+        'https://thumbs.dreamstime.com/b/adidas-logo-editorial-ilustrativo-sobre-fondo-blanco-eps-descargar-vector-jpeg-banner-208329055.jpg',
+        'Tienda de descuentos de Adidas',
+        3500
+    );
 
-SELECT * FROM posts;
+INSERT INTO posts (titulo, img, descripcion, likes)
+VALUES (
+        'Apple Store',
+        'https://i.pinimg.com/originals/5d/22/55/5d22555b9eccd85d6c478baae08ecb09.png',
+        'Tienda oficial de Apple',
+        8000
+    );
+
+
+SELECT *
+FROM posts;
